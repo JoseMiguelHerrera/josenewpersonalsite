1c9bdc99b2210be2db75a4b6601a967e53bbeded
Jose Herrera <josemiguelherrera@protonmail.com>
2024-10-08 16:56:49 -0400

    more meta tags

--- pages/index.js
+++ pages/index.js
@@ -37,11 +37,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Jose Herrera</title>
+        <title>Jose Herrera Web3 Software Engineer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <meta property="og:image" content="https://josenewpersonalsitehaxxor.vercel.app/ogPicture.png"/>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
